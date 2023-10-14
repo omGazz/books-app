6eef0f3abc413ebed827e2d28b7cd90577691c91
mgazzola <mgazzola@datacolor.com>
2023-10-14 21:26:56 +0200

    styled cover

--- components/book/book.js
+++ components/book/book.js
@@ -15,17 +15,25 @@ class Book extends HTMLElement {
     const shadow = this.attachShadow({ mode: "closed" });
     shadow.innerHTML = `<div class="book">
                             <h4>${this.title}</h4>
-                            <img class="image" src="${this.cover}"/>
+                            <img class="cover" src="${this.cover}"/>
                             <p class="description">${this.description}</p>
                         </div>
                         
                         <style>
                             .book {
+                                display: flex;
+                                align-items: center;
+                                flex-direction: column;
                                 padding: 15px;
+                                margin: 5px;
                                 width: 250px;
                                 height: 300px;
                                 border-radius: 10px;
                                 background-color: #F9F9F9;
+                            }
+
+                            .cover {
+                                width: 100%;
                             }
                         </style>`;
   }
