bec45673cc02030bb6040a8cf1a2c6eb094871d4
mgazzola <mgazzola@datacolor.com>
2023-10-14 21:15:12 +0200

    created book-component

--- components/book/book.js
+++ components/book/book.js
@@ -1,10 +1,34 @@
 class Book extends HTMLElement {
-    constructor() {
-        super();
-    }
-    connectedCallback() {
-        this.innerText = 'myBook';
-    }
+
+    static get observedAttributes() {
+        return ['title', 'cover', 'description'];
+      }
+
+  constructor() {
+    super();
+    this.title = this.getAttribute('title');
+    this.cover = this.getAttribute('cover');
+    this.description = this.getAttribute('description');
+  }
+
+  connectedCallback() {
+    const shadow = this.attachShadow({ mode: "closed" });
+    shadow.innerHTML = `<div class="book">
+                            <h4>${this.title}</h4>
+                            <img class="image" src="${this.cover}"/>
+                            <p class="description">${this.description}</p>
+                        </div>
+                        
+                        <style>
+                            .book {
+                                padding: 15px;
+                                width: 250px;
+                                height: 300px;
+                                border-radius: 10px;
+                                background-color: #F9F9F9;
+                            }
+                        </style>`;
+  }
 }
 
-customElements.define('book-component', Book);
+customElements.define("book-component", Book);
