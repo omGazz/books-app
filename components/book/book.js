0691789b62e9e13aae1c48d159bc9b999acec4e5
mgazzola <mgazzola@datacolor.com>
2023-10-15 12:31:07 +0200

    added covers and refactored component

--- components/book/book.js
+++ components/book/book.js
@@ -1,22 +1,19 @@
 class Book extends HTMLElement {
-
-    static get observedAttributes() {
-        return ['title', 'cover', 'description'];
-      }
+  static get observedAttributes() {
+    return ["title"];
+  }
 
   constructor() {
     super();
-    this.title = this.getAttribute('title');
-    this.cover = this.getAttribute('cover');
-    this.description = this.getAttribute('description');
+    this.cover = this.getAttribute("cover");
   }
 
   connectedCallback() {
     const shadow = this.attachShadow({ mode: "closed" });
     shadow.innerHTML = `<div class="book">
-                            <h4>${this.title}</h4>
-                            <img class="cover" src="${this.cover}"/>
-                            <p class="description">${this.description}</p>
+                            <div class="cover-container">
+                              <img class="cover" src="${this.cover}"/>
+                            </div>
                         </div>
                         
                         <style>
@@ -26,14 +23,19 @@ class Book extends HTMLElement {
                                 flex-direction: column;
                                 padding: 15px;
                                 margin: 5px;
-                                width: 250px;
+                                width: 300px;
                              
                                 border-radius: 10px;
-                                background-color: #F9F9F9;
+                                background-color: #333333;
                             }
 
                             .cover {
                                 width: 100%;
+                            }
+
+                            .cover-container {
+                              overflow: hidden;
+                              height: 460px;
                             }
                         </style>`;
   }
